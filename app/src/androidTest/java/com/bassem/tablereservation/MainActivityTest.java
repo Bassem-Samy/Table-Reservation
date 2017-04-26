d0111a5e262774cf1d0f1e80d273493090b9b092
Bassem Samy <bassem.samy@hotmail.com>
2017-04-26 23:11:03 +0200

    minor fixes

--- app/src/androidTest/java/com/bassem/tablereservation/MainActivityTest.java
+++ app/src/androidTest/java/com/bassem/tablereservation/MainActivityTest.java
@@ -1,8 +1,10 @@
 package com.bassem.tablereservation;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.content.ContextCompat;
 
 import com.bassem.tablereservation.ui.MainActivity;
 import com.bassem.tablereservation.utils.Constants;
@@ -19,6 +21,8 @@ import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
@@ -28,12 +32,10 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
  */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
-    static final int EXPECTED_ITEMS_COUNT = 14;
+    private static final int EXPECTED_CUSTOMERS_COUNT = 14;
     private static MockWebServer mockWebServer;
     private static final String ASSETS_CUSTOMERS_FILE_NAME = "customers_response.json";
     private static final String LAST_ITEM_EXPECTED_FULL_NAME = "Cleave Staples Lewis";
-
-
     @Rule
     public ActivityTestRule<MainActivity> mainActivityRule = new ActivityTestRule<>(MainActivity.class);
 
@@ -41,13 +43,17 @@ public class MainActivityTest {
         return AssetsFileReader.readFileAsString(ASSETS_CUSTOMERS_FILE_NAME);
     }
 
+
+
     private static void clearSearchEditText() {
         onView(withId(R.id.edt_filter)).perform(ViewActions.clearText());
     }
 
+
     @BeforeClass
     public static void setup() throws IOException {
         mockWebServer = new MockWebServer();
+
         mockWebServer.start();
         Constants.SERVICE_BASE_URL = mockWebServer.url("test/").toString();
         MockResponse mockResponse = new MockResponse().setResponseCode(200)
@@ -67,15 +73,17 @@ public class MainActivityTest {
     @Test
     public void checkCustomers() {
         // test number of items displayed
-        onView(withId(R.id.rclr_customers)).check(new RecyclerViewItemsCountAssertion(EXPECTED_ITEMS_COUNT));
+        onView(withId(R.id.rclr_customers)).check(new RecyclerViewItemsCountAssertion(EXPECTED_CUSTOMERS_COUNT));
         // scroll to last item
-        onView(withId(R.id.rclr_customers)).perform(scrollToPosition(EXPECTED_ITEMS_COUNT - 1));
+        onView(withId(R.id.rclr_customers)).perform(scrollToPosition(EXPECTED_CUSTOMERS_COUNT - 1));
         // check if the last item is the expected  name
         onView(withId(R.id.rclr_customers)).check(new RecyclerViewItemStringDataAssertion(
                 R.id.txt_full_name,
                 LAST_ITEM_EXPECTED_FULL_NAME,
-                EXPECTED_ITEMS_COUNT - 1)
+                EXPECTED_CUSTOMERS_COUNT - 1)
         );
+
+
     }
 
 
